61e9cfff3b10e5987980ebc0d43e02ab6a4ccd22
Pheotis <49039536+Pheotis@users.noreply.github.com>
2022-11-13 18:49:45 -0500

    Added web file structure

---
+++ src/main/javascript/cc/phantomhost/core/assets/js/main.js
@@ -0,0 +1,17 @@
+/*
+ *	Licensed by The Phantom Project under the Modelica License 2
+ *	Copyright (c) 2022 Thorinwasher on behalf of PMC's contributors.
+ *	
+ *	This program is free software and its use is completely at your
+ *	own risk; it can be redistributed and/or modified under the terms of the
+ *	Modelica License 2.
+ *	
+ *	For license conditions (including the disclaimer of warranty) see
+ *	http://www.modelica.org/licenses/ModelicaLicense2.
+ */
+ 
+ /**
+  *	Phantom Head Server
+  * 
+  * @author Thorin 
+  */
